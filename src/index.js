4d04460d80b8ff0915f365d9329772f02cab77e6
ingnyan <ingnyan@ed.team>
2021-06-17 17:27:10 -0500

    Provider added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import store from './redux/store';
 import './styles/styles.scss'
+import {Provider} from 'react-redux'
+// import { getAllSpecialities } from './redux/actionCreators';
 
+// store.dispatch(getAllSpecialities())
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
